7489314593d1662b7bf4c277fe77c4cdc8ed697e
Niranjan <niranjanbaviskar27@gmail.com>
2025-06-27 17:35:48 +0530

    updation

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const fetch = (...args) => import("node-fetch").then(({ default: fetch }) => fet
 require("dotenv").config();
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
 
 app.use(cors());
 app.use(bodyParser.json());
